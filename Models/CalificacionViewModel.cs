f09ba01442da4085669efb21c26401727e90e2d5
Omar Genao <omargenao78@gmail.com>
2025-06-26 19:38:32 -0400

    creacion materia model y calificacion , modificacion estudiante model

---
+++ Models/CalificacionViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Actividad4LengProg3.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LengProg3.Models
+{
+    public class CalificacionViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required string MatriculaEstudiante { get; set; }
+
+        [Required]
+        public required string CodigoMateria { get; set; }
+
+        [Required, Range(0, 100)]
+        public int Nota { get; set; }
+
+        [Required]
+        public required string Periodo { get; set; }
+
+        // Relaciones
+        [ForeignKey("MatriculaEstudiante")]
+        public required EstudianteViewModel Estudiante { get; set; }
+
+        [ForeignKey("CodigoMateria")]
+        public required MateriaViewModel Materia { get; set; }
+    }
+}
---
+++ Models/MateriaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LengProg3.Models
+{
+    public class MateriaViewModel
+    {
+        [Key]
+        [Required]
+        public required string Codigo { get; set; }
+
+        [Required, StringLength(100)]
+        public required string Nombre { get; set; }
+
+        [Required, Range(1, 10)]
+        public int Creditos { get; set; }
+
+        [Required]
+        public required string Carrera { get; set; }
+    }
+}
