febc14663fcd534ef0cbef079653af2023d98bb6
Omar Genao <omargenao78@gmail.com>
2025-06-30 00:11:12 -0400

    creacion controlador caalificaciones y materias, algunas modificaciones en ellos

---
+++ Controllers/CalificacionesController.cs
@@ -0,0 +1,91 @@
+﻿using Actividad4LengProg3.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad4LengProg3.Controllers
+{
+    public class CalificacionesController : Controller
+    {
+
+        private readonly EstudiantesDbContext _context;
+
+        public CalificacionesController(EstudiantesDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CalificacionesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CalificacionesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CalificacionesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CalificacionesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CalificacionesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CalificacionesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CalificacionesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/MateriasController.cs
@@ -0,0 +1,91 @@
+﻿using Actividad4LengProg3.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad4LengProg3.Controllers
+{
+    public class MateriasController : Controller
+    {
+
+        private readonly EstudiantesDbContext _context;
+
+        public MateriasController(EstudiantesDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MateriasController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MateriasController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MateriasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MateriasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MateriasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MateriasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MateriasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
